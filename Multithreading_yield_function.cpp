484061884e5660af3541fa987a06c22181e3ada6
Shidlingayya <65803331+shidlingayya@users.noreply.github.com>
2025-11-20 11:25:42 +0530

    Add files via upload

---
+++ Multithreading_yield_function.cpp
@@ -0,0 +1,22 @@
+// 1_singleThreaded.cpp
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+void little_sleep(std::chrono::microseconds us)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = start + us;
+    do
+    {
+        std::this_thread::yield();
+    } while (std::chrono::high_resolution_clock::now() < end);
+}
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    little_sleep(std::chrono::microseconds(100));
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    std::cout << "waited for " << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count() << " ms" << std::endl;
+}
